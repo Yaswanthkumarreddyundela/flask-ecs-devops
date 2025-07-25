27048c6cdd5c7cf1d3404d7d6bd499438a82c353
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-07-25 21:36:12 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,24 @@
 output "alb_dns" {
-  value = aws_lb.app_alb.dns_name
+  description = "DNS name of the load balancer"
+  value       = aws_lb.app_alb.dns_name
+}
+
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.repo.repository_url
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.app.name
+}
+
+output "task_definition_family" {
+  description = "Family name of the task definition"
+  value       = aws_ecs_task_definition.app.family
 }
