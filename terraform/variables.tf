5035ba43a55c608106e6582cc27ec3725f76bd47
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-07-25 21:35:47 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,23 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "project_name" {
-  default = "flask-todo-app"
+  description = "Name of the project"
+  type        = string
+  default     = "flask-todo-app"
+}
+
+variable "app_port" {
+  description = "Port the application runs on"
+  type        = number
+  default     = 5000
+}
+
+variable "desired_count" {
+  description = "Desired number of ECS tasks"
+  type        = number
+  default     = 1
 }
