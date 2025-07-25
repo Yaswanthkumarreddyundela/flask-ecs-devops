a5a7903437f66b6cc36c4d0faff904700f8e99c2
yaswanth reddy <118503416+Yaswanthkumarreddyundela@users.noreply.github.com>
2025-07-25 21:35:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,70 @@ terraform {
     }
   }
 }
+
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "app_logs" {
+  name              = "/ecs/${var.project_name}"
+  retention_in_days = 7
+}
+
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  name_prefix = "${var.project_name}-alb-"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg"
+  }
+}
+
+# Security Group for ECS Tasks
+resource "aws_security_group" "app_sg" {
+  name_prefix = "${var.project_name}-app-"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-sg"
+  }
+}
